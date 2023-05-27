f6106fcb78b47126a10198a66a0a3b11f6d06e39
杜江川 <894230813@qq.com>
2023-05-27 12:30:32 +0800

    导出包

--- lib/super_editor_json.dart
+++ lib/super_editor_json.dart
@@ -1,4 +1,5 @@
 library super_editor_json;
 
+export 'package:super_editor_json/parser/document_parser.dart';
 export 'package:super_editor_json/parser/json/document_json_parser.dart';
 export 'package:super_editor_json/ext/document_parser_ext.dart';
